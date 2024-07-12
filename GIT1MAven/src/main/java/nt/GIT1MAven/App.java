76ed0380024120a0e1841b3b681a9447f429fd8a
Narayana <Narayana@192.168.1.10>
2024-07-12 17:19:51 +0530

    initial structure

---
+++ GIT1MAven/src/main/java/nt/GIT1MAven/App.java
@@ -0,0 +1,13 @@
+package nt.GIT1MAven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
